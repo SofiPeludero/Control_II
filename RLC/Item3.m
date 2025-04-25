d942177c44fc2484909a0cbf0811dacb9d448dc2
SofiPeludero <89232784+SofiPeludero@users.noreply.github.com>
2025-04-24 22:12:02 -0300

    Update Item3.m

--- RLC/Item3.m
+++ RLC/Item3.m
@@ -1,21 +1,18 @@
 clear all; clc; 
 
-R=272,6843; L=0.1;C=9.8648e-6;
+R=272.6843; L=0.1;C=9.8648e-6;
 
-V0 = 12;       % Amplitud de la fuente de voltaje (puedes usar una fuente alterna)
+V0 = 12;       % Amplitud de la fuente de voltaje
 f = 10;       % Frecuencia de la fuente (en Hz)
 % Definir la función de voltaje 
 V = @(t) V0 * square(2 * pi * f * t);  % Fuente de voltaje sinusoidal
-% Definir las condiciones iniciales
-q0 = 0;     % Carga inicial en el condensador (C)
-i0 = 0;     % Corriente inicial (i(t) = dq/dt)
 
 % Definir el sistema de ecuaciones diferenciales
 odefun = @(t, y) [y(2); (V(t) - R * y(2) - y(1) / C) / L];
 
 % Resolver la ecuación diferencial
-tspan = [0.05 0.2];  % Tiempo de simulación (por ejemplo, de 0 a 0.1 segundos)
-y0 = [q0; i0];    % Condiciones iniciales (carga y corriente)
+tspan = [0.05 0.2];  % Tiempo de simulación 
+y0 = [0; 0];    % Condiciones iniciales (carga y corriente)
 [t, y] = ode45(odefun, tspan, y0);
 
 [z1]=xlsread('Curvas_Medidas_RLC_2024');
