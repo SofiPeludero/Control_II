453739cdf7aeb629cc7e3ee142549ba49f5d211e
SofiPeludero <89232784+SofiPeludero@users.noreply.github.com>
2025-06-13 17:45:45 -0300

    Update Item3.m

--- RLC/Item3.m
+++ RLC/Item3.m
@@ -1,17 +1,19 @@
 clear all; clc; 
 
-R=272.6843; L=0.1;C=9.8648e-6;
+%Valores calculados en el item anterior: 
+R=267.5231; L= 0.0981;C= 1.0055e-05;
+
 
 V0 = 12;       % Amplitud de la fuente de voltaje
 f = 10;       % Frecuencia de la fuente (en Hz)
-% Definir la función de voltaje 
+% Definir la funciÃ³n de voltaje 
 V = @(t) V0 * square(2 * pi * f * t);  % Fuente de voltaje sinusoidal
 
 % Definir el sistema de ecuaciones diferenciales
 odefun = @(t, y) [y(2); (V(t) - R * y(2) - y(1) / C) / L];
 
-% Resolver la ecuación diferencial
-tspan = [0.05 0.2];  % Tiempo de simulación 
+% Resolver la ecuaciÃ³n diferencial
+tspan = [0.01 0.2];  % Tiempo de simulaciÃ³n 
 y0 = [0; 0];    % Condiciones iniciales (carga y corriente)
 [t, y] = ode45(odefun, tspan, y0);
 
@@ -22,4 +24,5 @@ I=z1(:,2);
 hold on;
 plot(t0,I,'r');title('Corriente Inductor I_L');grid;
 plot(t, y(:, 2),'b');  % y(:, 2) es la corriente i(t)
-xlim([0.049 0.2]);
+xlim([0.0 0.2]);
+legend('Real','Modelo');
